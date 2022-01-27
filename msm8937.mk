11acf51a61713de21796714d3903eb578979a02f
Alexander Koskovich <akoskovich@pm.me>
2022-01-27 14:23:56 -0700

    msm8937-common: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).
    
     * Import GcamGo in device tree to aljeter
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- msm8937.mk
+++ msm8937.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -67,7 +67,6 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson.vendor \
-    Snap \
     libstdc++.vendor \
     libwui
 
