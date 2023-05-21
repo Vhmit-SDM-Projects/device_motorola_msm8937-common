7f951cf202b846e8b2990ebad120e470b1dfaa5e
Vhmit <praxedes.9630@gmail.com>
2023-05-21 20:13:07 -0300

    msm8937-common: Update script for update blobs
    Signed-off-by: Vhmit <praxedes.9630@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -7,13 +7,16 @@
 
 set -e
 
+DEVICE_COMMON=msm8937-common
+VENDOR=motorola
+
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -23,8 +26,19 @@ source "${HELPER}"
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
+ONLY_COMMON=
+ONLY_TARGET=
+KANG=
+SECTION=
+
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
+        --only-common )
+                ONLY_COMMON=true
+                ;;
+        --only-target )
+                ONLY_TARGET=true
+                ;;
         -n | --no-cleanup )
                 CLEAN_VENDOR=false
                 ;;
@@ -46,69 +60,69 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-# Load wrapped shim
 function blob_fixup() {
     case "${1}" in
-    lib64/libwfdnative.so)
-        patchelf --remove-needed android.hidl.base@1.0.so "${2}"
-        ;;
+        lib64/libwfdnative.so)
+            "${PATCHELF}" --remove-needed android.hidl.base@1.0.so "${2}"
+            ;;
 
-    product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
-        sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
-        ;;
+        product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
+            sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
+            ;;
 
-    # memset shim
-    vendor/bin/charge_only_mode)
-        patchelf --add-needed libmemset_shim.so "${2}"
-        ;;
+        # memset shim
+        vendor/bin/charge_only_mode)
+            "${PATCHELF}" --add-needed libmemset_shim.so "${2}"
+            ;;
 
-    vendor/lib/hw/activity_recognition.msm8937.so | vendor/lib64/hw/activity_recognition.msm8937.so)
-        patchelf --set-soname activity_recognition.msm8937.so "${2}"
-        ;;
+        vendor/lib/hw/activity_recognition.msm8937.so | vendor/lib64/hw/activity_recognition.msm8937.so)
+            "${PATCHELF}" --set-soname activity_recognition.msm8937.so "${2}"
+            ;;
 
-    vendor/lib/hw/camera.msm8937.so)
-        patchelf --set-soname camera.msm8937.so "${2}"
-        ;;
+        vendor/lib/hw/camera.msm8937.so)
+            "${PATCHELF}" --set-soname camera.msm8937.so "${2}"
+            ;;
 
-    vendor/lib64/hw/gatekeeper.msm8937.so)
-        patchelf --set-soname gatekeeper.msm8937.so "${2}"
-        ;;
+        vendor/lib64/hw/gatekeeper.msm8937.so)
+            "${PATCHELF}" --set-soname gatekeeper.msm8937.so "${2}"
+            ;;
 
-    vendor/lib64/hw/keystore.msm8937.so)
-        patchelf --set-soname keystore.msm8937.so "${2}"
-        ;;
+        vendor/lib64/hw/keystore.msm8937.so)
+            "${PATCHELF}" --set-soname keystore.msm8937.so "${2}"
+            ;;
 
-    vendor/lib/libactuator_dw9767_truly.so)
-        patchelf --set-soname libactuator_dw9767_truly.so "${2}"
-        ;;
+        vendor/lib/libactuator_dw9767_truly.so)
+            "${PATCHELF}" --set-soname libactuator_dw9767_truly.so "${2}"
+            ;;
 
-    vendor/lib/libmmcamera2_sensor_modules.so)
-        sed -i 's|msm8953_mot_deen_camera.xml|msm8937_mot_camera_conf.xml|g' "${2}"
-        ;;
+        # Fix camera recording
+        vendor/lib/libmmcamera2_sensor_modules.so)
+            sed -i 's|msm8953_mot_deen_camera.xml|msm8937_mot_camera_conf.xml|g' "${2}"
+            ;;
 
-    vendor/lib/libmot_gpu_mapper.so | vendor/lib/libmmcamera_vstab_module.so | vendor/lib/libjscore.so)
-        sed -i "s/libgui/libwui/" "${2}"
-        ;;
+        vendor/lib/libmot_gpu_mapper.so | vendor/lib/libmmcamera_vstab_module.so)
+            sed -i "s/libgui/libwui/" "${2}"
+            ;;
 
-    vendor/lib64/libmdmcutback.so)
-        sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "${2}"
-        ;;
+        vendor/lib64/libmdmcutback.so)
+            sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "${2}"
+            ;;
     esac
 }
 
-# Initialize the helper
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+if [ -z "${ONLY_TARGET}" ]; then
+    # Initialize the helper for common device
+    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
-        "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
 
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     source "${MY_DIR}/../${DEVICE}/extract-files.sh"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" \
-            "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
 "${MY_DIR}/setup-makefiles.sh"
