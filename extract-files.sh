96b2ba87e9585fbceb55e1dabc299ed4e20a2516
Vhmit <praxedes.9630@gmail.com>
2023-05-23 04:22:24 -0300

    update script

--- extract-files.sh
+++ extract-files.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
+
+DEVICE_COMMON=msm8937-common
+VENDOR=motorola
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -21,72 +23,18 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
-function blob_fixup() {
-    case "${1}" in
-        system_ext/etc/permissions/qcrilhook.xml)
-            sed -i "s|/product/framework/|/system_ext/framework/|g" "${2}"
-            ;;
-
-        system_ext/etc/permissions/telephonyservice.xml)
-            sed -i "s|/system/product/framework/|/system_ext/framework/|g" "${2}"
-            ;;
-
-        # memset shim
-        vendor/bin/charge_only_mode)
-            for LIBMEMSET_SHIM in $(grep -L "libmemset_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libmemset_shim.so" "${LIBMEMSET_SHIM}"
-            done
-            ;;
-
-        vendor/lib/hw/activity_recognition.msm8937.so | vendor/lib64/hw/activity_recognition.msm8937.so)
-            "${PATCHELF}" --set-soname activity_recognition.msm8937.so "${2}"
-            ;;
-
-        vendor/lib64/hw/gatekeeper.msm8937.so)
-            "${PATCHELF}" --set-soname gatekeeper.msm8937.so "${2}"
-            ;;
-
-        vendor/lib64/hw/keystore.msm8937.so)
-            "${PATCHELF}" --set-soname keystore.msm8937.so "${2}"
-            ;;
-
-        vendor/lib/libmot_gpu_mapper.so | vendor/lib/libmmcamera_vstab_module.so | vendor/lib/libjscore.so)
-            sed -i "s/libgui/libwui/" "${2}"
-            ;;
-
-        vendor/lib64/libmdmcutback.so)
-            sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "${2}"
-            ;;
-
-        vendor/lib64/libril-qc-qmi-1.so)
-            for LIBCUTILS_SHIM in $(grep -L "libcutils_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libcutils_shim.so" "${LIBCUTILS_SHIM}"
-            done
-            ;;
-
-        # Fix camera recording
-        vendor/lib/libmmcamera2_pproc_modules.so)
-            sed -i "s/ro.product.manufacturer/ro.product.nopefacturer/" "${2}"
-            ;;
-    esac
-}
-
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
-ONLY_BOARD_COMMON=
-ONLY_DEVICE_COMMON=
+ONLY_COMMON=
 ONLY_TARGET=
 KANG=
 SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        --only-board-common )
-                ONLY_BOARD_COMMON=true
-                ;;
-        --only-device-common )
-                ONLY_DEVICE_COMMON=true
+        --only-common )
+                ONLY_COMMON=true
                 ;;
         --only-target )
                 ONLY_TARGET=true
@@ -112,22 +60,52 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-if [ -z "${ONLY_TARGET}" ] && [ -z "${ONLY_DEVICE_COMMON}" ]; then
-    # Initialize the helper
-    setup_vendor "${BOARD_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
+function blob_fixup() {
+    case "${1}" in
+        lib64/libwfdnative.so)
+            "${PATCHELF}" --remove-needed android.hidl.base@1.0.so "${2}"
+            ;;
 
-    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
+        product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
+            sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
+            ;;
+
+        # memset shim
+        vendor/bin/charge_only_mode)
+            "${PATCHELF}" --add-needed libmemset_shim.so "${2}"
+            ;;
+
+        vendor/lib/hw/camera.msm8937.so)
+            "${PATCHELF}" --set-soname camera.msm8937.so "${2}"
+            ;;
+
+        vendor/lib/libactuator_dw9767_truly.so)
+            "${PATCHELF}" --set-soname libactuator_dw9767_truly.so "${2}"
+            ;;
+
+        # Fix camera recording
+        vendor/lib/libmmcamera2_sensor_modules.so)
+            sed -i 's|msm8953_mot_deen_camera.xml|msm8937_mot_camera_conf.xml|g' "${2}"
+            ;;
+
+        vendor/lib/libmot_gpu_mapper.so | vendor/lib/libmmcamera_vstab_module.so)
+            sed -i "s/libgui/libwui/" "${2}"
+            ;;
+
+        vendor/lib64/libmdmcutback.so)
+            sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "${2}"
+            ;;
+    esac
+}
+
+if [ -z "${ONLY_TARGET}" ]; then
+    # Initialize the helper for common device
+    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
+
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
-if [ -z "${ONLY_BOARD_COMMON}" ] && [ -z "${ONLY_TARGET}" ] && [ -s "${MY_DIR}/../${DEVICE_COMMON}/proprietary-files.txt" ];then
-    # Reinitialize the helper for device common
-    source "${MY_DIR}/../${DEVICE_COMMON}/extract-files.sh"
-    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
-
-    extract "${MY_DIR}/../${DEVICE_COMMON}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-fi
-
-if [ -z "${ONLY_BOARD_COMMON}" ] && [ -z "${ONLY_DEVICE_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     source "${MY_DIR}/../${DEVICE}/extract-files.sh"
     setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-# Override anything that may come from the calling environment
-BOARD=msm8937
+DEVICE_COMMON=msm8937-common
+VENDOR=motorola
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -24,38 +23,17 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
-# Store device common variable for later usage
-DEVICE_COMMON_ORIGINAL="${DEVICE_COMMON}"
-DEVICE_COMMON="${BOARD_COMMON}"
-
-# Initialize the helper
-setup_vendor "${BOARD_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
+# Initialize the helper for common
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "${BOARD}" "TARGET_BOARD_PLATFORM"
+write_headers "ahannah cedric hannah james montana rhannah aljeter"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 # Finish
 write_footers
-
-# Restore device common variable to original one
-DEVICE_COMMON="${DEVICE_COMMON_ORIGINAL}"
-
-if [ -s "${MY_DIR}/../${DEVICE_COMMON}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device common
-    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
-
-    # Warning headers and guards
-    write_headers "${DEVICE_COMMON_GUARDS}"
-
-    # The standard device common blobs
-    write_makefiles "${MY_DIR}/../${DEVICE_COMMON}/proprietary-files.txt" true
-
-    # Finish
-    write_footers
-fi
 
 if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
