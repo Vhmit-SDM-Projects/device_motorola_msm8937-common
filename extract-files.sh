24a3da2f44f2cca72ba66cea0d5e887df48ca352
Vhmit <praxedes.9630@gmail.com>
2023-05-24 09:36:45 -0300

    msm8937-common: Update scripts for update blobs
    Signed-off-by: Vhmit <praxedes.9630@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,9 @@
 
 set -e
 
+DEVICE_COMMON=msm8937-common
+VENDOR=motorola
+
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
@@ -74,19 +77,15 @@ function blob_fixup() {
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
-ONLY_BOARD_COMMON=
-ONLY_DEVICE_COMMON=
+ONLY_COMMON=
 ONLY_TARGET=
 KANG=
 SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        --only-board-common )
-                ONLY_BOARD_COMMON=true
-                ;;
-        --only-device-common )
-                ONLY_DEVICE_COMMON=true
+        --only-common )
+                ONLY_COMMON=true
                 ;;
         --only-target )
                 ONLY_TARGET=true
@@ -107,27 +106,18 @@ while [ "${#}" -gt 0 ]; do
     esac
     shift
 done
-
 if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-if [ -z "${ONLY_TARGET}" ] && [ -z "${ONLY_DEVICE_COMMON}" ]; then
-    # Initialize the helper
-    setup_vendor "${BOARD_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
+if [ -z "${ONLY_TARGET}" ]; then
+    # Initialize the helper for common device
+    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
-if [ -z "${ONLY_BOARD_COMMON}" ] && [ -z "${ONLY_TARGET}" ] && [ -s "${MY_DIR}/../${DEVICE_COMMON}/proprietary-files.txt" ];then
-    # Reinitialize the helper for device common
-    source "${MY_DIR}/../${DEVICE_COMMON}/extract-files.sh"
-    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
-
-    extract "${MY_DIR}/../${DEVICE_COMMON}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-fi
-
-if [ -z "${ONLY_BOARD_COMMON}" ] && [ -z "${ONLY_DEVICE_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     source "${MY_DIR}/../${DEVICE}/extract-files.sh"
     setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
