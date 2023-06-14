96462f92ff93c99a2c789117863d8ef7df3b459b
Vhmit <praxedes.9630@gmail.com>
2023-06-14 13:28:16 -0300

    msm8937-common: Fix and Convert sensor for 64b
    Signed-off-by: Vhmit <praxedes.9630@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -333,8 +333,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-impl:64 \
+    android.hardware.sensors@1.0-service
 
 # Shims
 PRODUCT_PACKAGES += \
