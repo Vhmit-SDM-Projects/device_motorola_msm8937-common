0d7d8fe5effb5bb7ba25ecf891447a0b67dc627c
Wiktor Strzębała <wiktorek140@tlen.pl>
2023-06-14 13:29:30 -0300

    msm8937-common: Set kernel version to 4.9
    Signed-off-by: Wiktor Strzębała <wiktorek140@tlen.pl>
    Change-Id: I863471d019917cad50095d427849bdc991f8baab

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,7 +111,7 @@ TARGET_FS_CONFIG_GEN += \
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=400M
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=400M 
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -119,7 +119,7 @@ BOARD_RAMDISK_OFFSET := 0x1000000
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-
+TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
 
 # Media
